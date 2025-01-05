9331c8060ef6a43c62b578c37ce7cc3a234ab9ee
jossanchezal <jossanchezal@unal.edu.co>
2025-01-05 18:07:06 -0500

    Merge branch 'main' of https://github.com/POO-2024-02-UNALMED/pr-ctica-1-grupo-1-equipo-7

--- src/gestorAplicacion/ui/uiEnviarPedidos.java
+++ src/gestorAplicacion/ui/uiEnviarPedidos.java
@@ -12,45 +12,48 @@ public class uiEnviarPedidos {
             
             int seleccion = -1;
             while(true){
-                while (true) {
-                    try {
-                        seleccion = sc.nextInt();
-                        if (seleccion == 0) {
-                            System.out.println("Saliendo...");
-                            sc.close();
-                            return;
+                Cliente clienteSeleccionado;
+                int confirmacionCliente = 0;
+                while(confirmacionCliente == 0){
+                    while (true) {
+                        try {
+                            seleccion = sc.nextInt();
+                            if (seleccion == 0) {
+                                System.out.println("Saliendo...");
+                                sc.close();
+                                return;
+                            } 
+                            else if (seleccion > 0 && seleccion <= Cliente.listaClientes.size()){
+                                break;
+                            } 
+                            else {
+                                System.out.println("Número fuera de rango. Por favor, elija un cliente válido.");
+                            }
                         } 
-                        else if (seleccion > 0 && seleccion <= Cliente.listaClientes.size()){
+                        catch (Exception e) {
+                            System.out.println("Entrada inválida. Por favor, ingrese un número.");
+                            sc.nextLine();
+                        }
+                    }
+                    System.out.println("Ha seleccionado el cliente: "+ Cliente.getListaClientes().get(seleccion - 1).getNombre());
+                    System.out.println("Para confirmar, ingrese 'Aceptar'. Para regresar al menú anterior, ingrese 'Regresar'.");
+                    
+                    while (true) {
+                        String eleccion = sc.next().toLowerCase();
+        
+                        if (eleccion.equals("aceptar")) {
+                            clienteSeleccionado = Cliente.getListaClientes().get(seleccion - 1);
+                            System.out.println("Cliente confirmado: " + clienteSeleccionado.getNombre());
+                            confirmacionCliente = 1;
+                            break;
+                        } 
+                        else if (eleccion.equals("regresar")) {
+                            System.out.println("Regresando al menú anterior...");
                             break;
                         } 
                         else {
-                            System.out.println("Número fuera de rango. Por favor, elija un cliente válido.");
+                            System.out.println("Por favor, ingrese 'Aceptar' para confirmar o 'Regresar' para volver al menú anterior.");
                         }
-                    } 
-                    catch (Exception e) {
-                        System.out.println("Entrada inválida. Por favor, ingrese un número.");
-                        sc.nextLine();
-                    }
-                }
-                System.out.println("Ha seleccionado el cliente: "+ Cliente.getListaClientes().get(seleccion - 1).getNombre());
-                System.out.println("Para confirmar, ingrese 'Aceptar'. Para regresar al menú anterior, ingrese 'Regresar'.");
-                
-                Cliente clienteSeleccionado;
-                
-                while (true) {
-                    String eleccion = sc.next().toLowerCase();
-    
-                    if (eleccion.equals("aceptar")) {
-                        clienteSeleccionado = Cliente.getListaClientes().get(seleccion - 1);
-                        System.out.println("Cliente confirmado: " + clienteSeleccionado.getNombre());
-                        break;
-                    } 
-                    else if (eleccion.equals("regresar")) {
-                        System.out.println("Regresando al menú anterior...");
-                        break;
-                    } 
-                    else {
-                        System.out.println("Por favor, ingrese 'Aceptar' para confirmar o 'Regresar' para volver al menú anterior.");
                     }
                 }
                 System.out.println("Seleccione la tienda desde la cual se enviará el pedido. Si no desea continuar, presione 0 para salir.");
@@ -60,28 +63,50 @@ public class uiEnviarPedidos {
 
                 int opcion = -1;
                 Tienda tiendaSeleccionada;
-                while (true) {
-                    try {
-                        opcion = sc.nextInt();
-                        if (opcion == 0) {
-                            System.out.println("Saliendo...");
-                            sc.close();
-                            return;
+                int confirmacionTienda = 0;
+                while(confirmacionTienda == 0){
+                    while (true) {
+                        try {
+                            opcion = sc.nextInt();
+                            if (opcion == 0) {
+                                System.out.println("Saliendo...");
+                                sc.close();
+                                return;
+                            } 
+                            else if (opcion > 0 && opcion <= Fabrica.getListaTienda().size()){
+                                tiendaSeleccionada = Fabrica.getListaTienda().get(opcion - 1);
+                                break;
+                            } 
+                            else {
+                                System.out.println("Número fuera de rango. Por favor, elija una tienda válida.");
+                            }
                         } 
-                        else if (opcion > 0 && opcion <= Fabrica.getListaTienda().size()){
+                        catch (Exception e) {
+                            System.out.println("Entrada inválida. Por favor, ingrese un número.");
+                            sc.nextLine();
+                        }
+                    }
+                    System.out.println("Tienda Seleccionada: "+ tiendaSeleccionada.getNombre());
+                    System.out.println("Para confirmar la tienda seleccionada, escriba 'Aceptar'. Si desea cambiar su selección, escriba 'Regresar'.");
+                    
+                    while (true) {
+                        String eleccion = sc.next().toLowerCase();
+        
+                        if (eleccion.equals("aceptar")) {
                             tiendaSeleccionada = Fabrica.getListaTienda().get(opcion - 1);
+                            System.out.println("Tienda confirmada: " + tiendaSeleccionada.getNombre());
+                            confirmacionTienda = 1;
+                            break;
+                        } 
+                        else if (eleccion.equals("regresar")) {
+                            System.out.println("Regresando al menú anterior...");
                             break;
                         } 
                         else {
-                            System.out.println("Número fuera de rango. Por favor, elija una tienda válida.");
+                            System.out.println("La opción ingresada no es válida. Por favor, ingrese 'aceptar' para confirmar la tienda seleccionada o 'regresar' para cambiarla.");
                         }
-                    } 
-                    catch (Exception e) {
-                        System.out.println("Entrada inválida. Por favor, ingrese un número.");
-                        sc.nextLine();
                     }
                 }
-                System.out.println("Tienda Seleccionada: "+ tiendaSeleccionada.getNombre());
 
             }
         }
